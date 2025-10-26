298673559c59bc2efe46d5dc5e4b600d595601b2
AshB <ashbasn@gmail.com>
2025-10-26 11:45:23 -0500

    Lists

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,16 @@
-friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+
+import random
+friends_list = ["Alice", "Jonn", "Bob", "Tom"]
+
+# Option #1
+length_of_list = len(friends_list)
+print(length_of_list)
+random_number = random.randint(0,length_of_list-1)
+print(random_number)
+
+print(friends_list[random_number])
+
+# Option #2
+print(random.choice(friends_list))
+
---
+++ Day 4/Lists/task.py
@@ -0,0 +1,8 @@
+import random
+friends_list = ["Alice", "Jonn", "Bob", "Tom"]
+length_of_list = len(friends_list)
+print(length_of_list)
+random_number = random.randint(0,length_of_list)
+print(random_number)
+
+print(friends_list[random_number])
