f638847d917c271e886600a1c9dbf20925b793ea
AshB <ashbasn@gmail.com>
2025-10-25 22:33:02 -0500

    elif

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,22 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+price = 0
+
+if size == 'S':
+    price = 15
+    if pepperoni == 'Y':
+        price += 2
+elif size == 'M':
+    price = 20
+    if pepperoni == 'Y':
+        price += 3
+elif size == 'L':
+    price = 25
+    if pepperoni == 'Y':
+        price += 3
+
+if extra_cheese == 'Y':
+    price += 1
+print(f"Your final bill is: ${price}.")
+
