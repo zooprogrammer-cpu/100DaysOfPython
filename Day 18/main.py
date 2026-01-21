fb338398024a39ae9ad3ca27369383bdc7cdcbf5
ashb <ashbasn@gmail.com>
2026-01-21 08:29:21 -0600

    Different shapes abd colors of turtle

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,14 +1,23 @@
 # Day 18 - Turtle Graphics
 # Check git setup
 from turtle import Turtle, Screen
+import random
 
+
+colors = ["CornFlowerBlue", "DarkOrchid", "IndianRed"]
 tim = Turtle()
 tim.shape("turtle")
-tim.color("red")
 
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+def draw_shape(number_of_sides):
+    angle = 360 / number_of_sides
+    for _ in range(number_of_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side)
 
 screen = Screen()
 
